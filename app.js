55a852e23b6c5ecf04afcf917c56b8ea57ac72e9
Mykhailo Tsynkevych <tsynkevych@gmx.de>
2023-06-27 08:15:07 +0200

    4

--- app.js
+++ app.js
@@ -19,19 +19,51 @@ export default class {
     this.createObject();
   }
 
-  createObject() {
-    this.object = new THREE.Mesh(
-      new THREE.BoxGeometry(1, 1, 1),
-      new THREE.MeshStandardMaterial({ color: "gray" })
-    );
-    Viewer.scene.add(this.object);
-
-    this.object.position.z = -5;
-
-    var that = this;
-
-    Viewer.addUpdate("rotate_object", () => {
-      that.object.rotation.y += 0.005;
-    });
-  }
+	createObject(){
+		this.object = new THREE.Mesh(
+			new THREE.BoxGeometry(.1,.1,1),
+			new THREE.MeshStandardMaterial({color:"gray"})
+		)
+		
+		Viewer.scene.add(this.object);
+		
+		
+		
+		var that = this;
+		
+		this.object.visible = true;
+		
+		var cone =  new THREE.Mesh(
+			new THREE.ConeGeometry(.1,.2,8),
+			new THREE.MeshStandardMaterial({color:"red"})
+		)
+		cone.position.z = -.5;
+		cone.rotation.x = -Math.PI/2;
+		
+		cone.name = "cone1";
+		
+		this.object.add(cone);
+		
+		
+		this.object.scale.set(1,3,4);
+		
+		var sphere =  new THREE.Mesh(
+			new THREE.SphereGeometry(.5,8,8),
+			new THREE.MeshStandardMaterial({color:"green"})
+		)
+		
+		sphere.position.set(5,0,-6);
+		
+		Viewer.scene.add(sphere);
+		
+		
+		Viewer.addUpdate("look_at_sphere",()=>{
+			sphere.position.x -= .05;
+			
+			that.object.lookAt(sphere.position.clone().multiplyScalar(-1));
+		})
+		
+		this.object.geometry
+		this.object.material
+	}
 }
--- viewer.js
+++ viewer.js
@@ -27,11 +27,6 @@ export default {
 
     this.renderer.setPixelRatio(settings.pixelRatio ?? devicePixelRatio);
 
-    this.renderer.setSize(
-      document.body.offsetWidth,
-      document.body.offsetHeight
-    );
-
     var that = this;
 
     this.addResize("resize_render", () => {
@@ -43,73 +38,74 @@ export default {
 
     this.resizePool["resize_render"]();
   },
-
-  createResize() {
-    var that = this;
-    window.addEventListener("resize", () => {
-      that.resize();
-    });
-  },
-
-  resizePool: {},
-  addResize(name, func) {
-    this.resizePool[name] = func;
-  },
-
-  removeResize(name) {
-    delete this.resizePool[name];
-  },
-
-  resize() {
-    for (var key in this.resizePool) this.resizePool[key]();
-  },
-
-  createCamera() {
-    this.camera = new THREE.PerspectiveCamera(
-      45,
-      this.renderer.domElement.offsetWidth /
-        this.renderer.domElement.offsetHeight,
-      1,
-      100
-    );
-
-    var that = this;
-
-    this.addResize("resize_camera", () => {
-      that.camera.aspect =
-        that.renderer.domElement.width / that.renderer.domElement.height;
-
-        that.camera.updateProjectionMatrix();
-    });
-  },
-
-  createScene() {
-    this.scene = new THREE.Scene();
-  },
-
-  createLight() {
-    this.light1 = new THREE.DirectionalLight(0xffffff, 0.5);
-    this.scene.add(this.light1);
-    this.light1.position.set(5, 5, 5);
-  },
-
-  addUpdate(name, func) {
-    this.updatePool[name] = func;
-  },
-
-  removeUpdate(name) {
-    delete this.updatePool[name];
-  },
-
-  updatePool: {},
-
-  update() {
-    this.renderer.render(this.scene, this.camera);
-    let that = this;
-    requestAnimationFrame(() => {
-      that.update();
-    });
-
-    for (let key in this.updatePool) this.updatePool[key]();
-  },
+  
+  createResize(){
+		var that = this;
+		window.addEventListener("resize",()=>{that.resize();});
+		
+		
+	},
+	resizePool:{},
+	addResize(name,func){
+		this.resizePool[name] = func;
+	},
+	removeRize(name){
+		delete this.resizePool[name];
+	},
+	resize(){
+		for(var key in this.resizePool)this.resizePool[key]();
+	},
+	createCamera(){
+		
+		this.camera = new THREE.PerspectiveCamera(
+			45,
+			this.renderer.domElement.width/this.renderer.domElement.height,
+			1,
+			100
+		)
+		
+		this.camera.position.set(5,10,10);
+		this.camera.lookAt(0,0,0);
+		
+		var that = this;
+	
+		this.addResize("resize_camera",()=>{
+			that.camera.aspect = that.renderer.domElement.width/that.renderer.domElement.height;
+			
+			that.camera.updateProjectionMatrix();
+		});
+	},
+	createScene(){
+		
+		this.scene = new THREE.Scene();
+	},
+	createLight(){
+		this.light1 = new THREE.DirectionalLight(0xffffff,.5);
+		
+		this.scene.add(this.light1);
+		
+		
+		this.light1.position.set(-1,3,10);
+		
+		this.light2 = new THREE.AmbientLight(0xffffff,.5);
+		
+		this.scene.add(this.light2);
+	},
+	addUpdate(name,func){
+		this.updatePool[name] = func;
+	},
+	removeUpdate(name){
+		delete this.updatePool[name];
+	},
+	updatePool : {},
+	update(){
+		
+		this.renderer.render(this.scene,this.camera);
+		
+		var that = this;
+		
+		requestAnimationFrame(()=>{that.update();});
+		
+		for(var key in this.updatePool)this.updatePool[key]();
+	}
 };
